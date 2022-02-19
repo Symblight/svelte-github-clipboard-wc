6c85c32a10ef6d53a0c80f702232b791cd47b0c5
Alexey <symblight@gmail.com>
2022-02-19 21:28:47 +0300

    init

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script context="module">
+import '@github/clipboard-copy-element'
+</script>
+
+
+<script>
+    export let name = "Alexis";
+</script>
+  <p>Hello {name}</p>
+
+
+<clipboard-copy for="blob-path">
+    <button>Copy</button>
+</clipboard-copy>
+<input id="blob-path" value="src/index.js">
